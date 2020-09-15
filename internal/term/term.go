d4abe2c3eef6c1cf77f0b19d7ab026204f8ff049
kuza <k.v-v@tut.by>
2020-09-15 11:47:46 +0200

    syscall.Termios -> unix.Termios

--- internal/term/term.go
+++ internal/term/term.go
@@ -7,15 +7,16 @@ import (
 	"syscall"
 
 	"github.com/pkg/term/termios"
+	"golang.org/x/sys/unix"
 )
 
 var (
-	saveTermios     syscall.Termios
+	saveTermios     unix.Termios
 	saveTermiosFD   int
 	saveTermiosOnce sync.Once
 )
 
-func getOriginalTermios(fd int) (syscall.Termios, error) {
+func getOriginalTermios(fd int) (unix.Termios, error) {
 	var err error
 	saveTermiosOnce.Do(func() {
 		saveTermiosFD = fd
